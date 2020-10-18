adb1b535c1ca3e284a90bcb03a38a9e62df304e3
firdyfirdy <trifirdyanto@gmail.com>
2020-10-18 08:09:49 +0700

    First Commit

---
+++ instagram-auto-unfollow/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace instagram_auto_unfollow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
